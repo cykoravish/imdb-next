2defbc4838acaae6429ba89cee84477cc4161104
Ravish Bisht <ravishbisht85@gmail.com>
2024-03-12 09:33:05 +0530

    fetching api with secret key

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,23 +1,23 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
-import Header from "@/components/Header";
-import Providers from "./Providers";
-import Navbar from "@/components/Navbar";
+import { Inter } from 'next/font/google';
+import './globals.css';
+import Header from '@/components/Header';
+import Providers from './Providers';
+import Navbar from '@/components/Navbar';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: "IMDB clone",
-  description: "A movie database",
+  title: 'IMDb clone',
+  description: 'This is a movie database clone',
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={inter.className}>
         <Providers>
           <Header />
-          <Navbar/>
+          <Navbar />
           {children}
         </Providers>
       </body>
