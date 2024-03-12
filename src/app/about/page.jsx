e8b18ba56df12bcf9b62217be65e3b41a295112f
Ravish Bisht <ravishbisht85@gmail.com>
2024-03-12 07:50:21 +0530

    about page add

---
+++ src/app/about/page.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+      Movie websites serve as digital hubs for film enthusiasts, offering a vast array of information about upcoming releases, trailers, reviews, and behind-the-scenes content. These platforms often feature user-friendly interfaces, making it easy for visitors to navigate and explore various genres, actors, and directors. With interactive features like personalized recommendations and integrated ticket booking, movie websites enhance the overall cinematic experience for audiences worldwide.
+      </p>
+      <p>
+      A well-designed movie website captivates users with visually stunning graphics, enticing trailers, and engaging content. From blockbuster hits to indie gems, these platforms showcase a diverse range of films, catering to every taste and preference. Advanced search filters and curated lists help users discover new favorites, while interactive forums and social media integration foster a sense of community among movie buffs, encouraging discussions and debates about the latest releases and timeless classics.
+      </p>
+      <p>
+      Behind the scenes, movie websites rely on cutting-edge technology and data analytics to optimize user experience and drive engagement. By analyzing user behavior and preferences, these platforms continuously evolve, offering personalized recommendations and tailored content to keep audiences hooked. With mobile-friendly designs and seamless integration across devices, movie websites ensure that film enthusiasts can access their favorite content anytime, anywhere, fostering a global community of passionate cinema lovers.
+      </p>
+    </div>
+  );
+}
